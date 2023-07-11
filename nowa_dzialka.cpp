eb5093258e89e8e10e48034dadd4182b88fd3fde
Krecikkko <80425089+Krecikkko@users.noreply.github.com>
2023-07-11 22:03:09 +0200

    Create nowa_dzialka.cpp

---
+++ nowa_dzialka.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int D,X;
+
+    cin >> D;
+
+    while (D--)
+    {
+        cin >> X;
+        cout << pow(X, 2) << endl;
+    }
+
+    return 0;
+}
