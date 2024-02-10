b6f52118d3d00c8cf51c51f67524f9a7749f5cb4
Krecikkko <80425089+Krecikkko@users.noreply.github.com>
2024-02-10 23:46:52 +0100

    Create SUMA - Suma.cpp

---
+++ SUMA - Suma.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+    int sum=0;
+
+    while (cin >> N) {
+        sum += N;
+        cout << sum << endl;
+    }
+    return 0;
+}
