1f72232c1fe037721a3c3f279d2ca231a0ae1fa8
Krecikkko <qubaqq2015@gmail.com>
2026-02-27 13:36:44 +0000

    Further work

--- BAJTELEK - Rysunki Bajtelka.cpp
+++ BAJTELEK - Rysunki Bajtelka.cpp
@@ -14,6 +14,15 @@ struct Coord
 struct Polyline
 {
     vector<Coord> points;
+
+    float calculateArea()
+    {
+        float fArea = 0.5 * ((points[1].x - points[0].x) * (points[2].y - points[0].y) -
+            (points[1].y - points[0].y) * (points[2].x - points[0].y));
+        float sArea = 0.5 * ((points[3].x - points[2].x) * (points[0].y - points[2].y) -
+            (points[3].y - points[2].y) * (points[0].x - points[2].y));
+        float area = fArea + sArea;
+    }
 };
 
 struct Drawing
@@ -25,6 +34,8 @@ public:
     {
         polylines.push_back(polyline);
     }
+
+    float calculateBlackArea() {}
 };
 
 int main() {
@@ -32,27 +43,31 @@ int main() {
 
     cin >> n;
 
-    Drawing drawing;
-    for (int i = 0; i < 2 * n; i++)
+    // Drawing drawing;
+    for (int i = 0; i < n; i++)
     {
-        Coord* firstCord = nullptr;
-        Polyline polyline;
-        while (true)
+        Drawing drawing;
+        for (int j = 0; j < 2; j++)
         {
-            Coord coord;
-            cin >> coord.x >> coord.y;
+            Coord* firstCord = nullptr;
+            Polyline polyline;
+            while (true)
+            {
+                Coord coord;
+                cin >> coord.x >> coord.y;
 
-            if (firstCord == nullptr)
-            {
-                firstCord = &polyline.points.back();
-            } else if (firstCord->x == coord.x && firstCord->y == coord.y)
-            {
-                break;
+                if (firstCord == nullptr)
+                {
+                    firstCord = &polyline.points.back();
+                } else if (firstCord->x == coord.x && firstCord->y == coord.y)
+                {
+                    break;
+                }
+
+                polyline.points.push_back(coord);
             }
-
-            polyline.points.push_back(coord);
+            drawing.addPolyline(polyline);
         }
-        drawing.addPolyline(polyline);
     }
 
     return 0;
